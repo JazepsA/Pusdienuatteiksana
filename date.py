476c5ccf909b399412119337c15a7fa8b0b9fade
JazepsA <jazepsaugustans26@gmail.com>
2025-01-22 14:24:45 +0200

    datetime prototip

--- date.py
+++ date.py
@@ -1,4 +1,27 @@
-import datetime
+from datetime import datetime
 
-d = datetime.date(2025, 1, 25)
-print(d)
+
+
+datestring = input("Please enter first a date in form dd/mm/yyyy: ")
+
+
+try:
+    date = datetime.strptime(datestring, '%d/%m/%Y')
+
+
+
+except ValueError:
+    print("Invalid date entered!")
+    
+datestring2= input("Please enter second a date in form dd/mm/yyyy: ")
+try:
+
+    date2 = datetime.strptime(datestring2, '%d/%m/%Y')
+    print(date)
+    print(date2)
+    starp=date2 - date
+    print("Difference in dates:", starp)
+
+except ValueError:
+    print("Invalid date entered!")
+
